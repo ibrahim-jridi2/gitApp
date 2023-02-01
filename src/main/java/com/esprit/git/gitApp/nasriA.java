4459f765fbcfdcdfe87fb5465c44e9d823cd0e60
i.jridi <ibrahim.jridi@esprit.tn>
2023-02-01 15:31:50 +0100

    neya sala7et khedmet amani

--- src/main/java/com/esprit/git/gitApp/nasriA.java
+++ src/main/java/com/esprit/git/gitApp/nasriA.java
@@ -4,3 +4,8 @@ public class nasriA {
     public void returnMsg(){
         System.out.println("this is ibrah first commit");
 }
+    public void returnMsg1(){
+        System.out.println("this is ibrahim1 first commit");
+    }
+
+}
